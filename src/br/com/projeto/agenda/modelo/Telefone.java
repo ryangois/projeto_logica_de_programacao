45b546e09ae7ad1ebaf8608a81f3dd7a196185e8
ryangois <ryangoisdev@gmail.com>
2024-01-29 18:35:42 -0300

    Estruturamento de pastas

---
+++ src/br/com/projeto/agenda/modelo/Telefone.java
@@ -0,0 +1,42 @@
+package br.com.projeto.agenda.modelo;
+
+public class Telefone {
+    private Long id;
+    private String ddd;
+    private Long numero;
+
+    public Telefone(Long id, String ddd, Long numero) {
+        this.id = id;
+        this.ddd = ddd;
+        this.numero = numero;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDdd() {
+        return ddd;
+    }
+
+    public void setDdd(String ddd) {
+        this.ddd = ddd;
+    }
+
+    public Long getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Long numero) {
+        this.numero = numero;
+    }
+
+    @Override
+    public String toString() {
+        return ddd + "-" + numero;
+    }
+}
