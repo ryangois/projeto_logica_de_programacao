282727096b34fac60938b7dad327484776a7c8f1
ryangois <ryangoisdev@gmail.com>
2024-01-29 18:35:27 -0300

    Estruturamento de pastas

--- src/br/com/projeto/agenda/Main.java
+++ src/br/com/projeto/agenda/Main.java
@@ -1,9 +1,15 @@
 package br.com.projeto.agenda;
 
+import br.com.projeto.agenda.io.AgendaManipulacao;
+import br.com.projeto.agenda.modelo.Contato;
+
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
+        }
 
-    }
+
 
 }
