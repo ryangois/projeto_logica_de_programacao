c85e2df85585acffbd75817fb86f4d290ac37a86
ryangois <ryangoisdev@gmail.com>
2024-01-29 21:32:17 -0300

    Terminal colorido

--- src/br/com/projeto/agenda/Agenda.java
+++ src/br/com/projeto/agenda/Agenda.java
@@ -9,7 +9,16 @@ import java.util.List;
 import java.util.Scanner;
 
 public class Agenda {
-
+    // Códigos de cores ANSI
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
     private List<Contato> contatos;
 
     public Agenda(List<Contato> contatos) {
@@ -21,14 +30,24 @@ public class Agenda {
     }
 
     public void adicionarContato(Contato contato) {
-        if (!contatos.contains(contato)) {
+        if (!contatos.contains(contato) && !temContatoComMesmosTelefones(contato)) {
             contatos.add(contato);
             AgendaManipulacao.salvarEmArquivo(contatos);
-            System.out.println("Contato adicionado com sucesso!");
+            System.out.println(GREEN + "Contato adicionado com sucesso!");
         } else {
-            System.out.println("Erro: Já existe um contato com o mesmo ID.");
+            System.out.println(RED + "Erro: Já existe um contato com o mesmo ID ou telefones duplicados.");
         }
     }
+
+    private boolean temContatoComMesmosTelefones(Contato novoContato) {
+        for (Contato contatoExistente : contatos) {
+            if (contatoExistente.temMesmosTelefones(novoContato)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 
     public void removerContato(Long id) {
         Contato contatoRemover = null;
@@ -42,9 +61,9 @@ public class Agenda {
         if (contatoRemover != null) {
             contatos.remove(contatoRemover);
             AgendaManipulacao.salvarEmArquivo(contatos);
-            System.out.println("Contato removido com sucesso!");
+            System.out.println(GREEN + "Contato removido com sucesso!");
         } else {
-            System.out.println("Erro: Contato não encontrado com o ID fornecido.");
+            System.out.println(RED + "Erro: Contato não encontrado com o ID fornecido.");
         }
     }
 
@@ -60,10 +79,10 @@ public class Agenda {
         if (contatoEditar != null) {
             contatos.remove(contatoEditar);
 
-            System.out.println("Informe o novo nome do contato: ");
+            System.out.println(BLUE + "Informe o novo nome do contato: ");
             String novoNome = scanner.nextLine();
 
-            System.out.println("Informe o novo sobrenome do contato: ");
+            System.out.println(BLUE + "Informe o novo sobrenome do contato: ");
             String novoSobrenome = scanner.nextLine();
 
             // Atualizar os dados do contato
@@ -75,9 +94,9 @@ public class Agenda {
 
             contatos.add(contatoEditar);
             AgendaManipulacao.salvarEmArquivo(contatos);
-            System.out.println("Contato editado com sucesso!");
+            System.out.println(GREEN + "Contato editado com sucesso!");
         } else {
-            System.out.println("Erro: Contato não encontrado com o ID fornecido.");
+            System.out.println(RED + "Erro: Contato não encontrado com o ID fornecido.");
         }
     }
 
@@ -100,96 +119,9 @@ public class Agenda {
             contato.adicionarTelefone(telefone);
         }
     }
-
     public void exibirContatos() {
         for (Contato contato : contatos) {
             System.out.println(contato);
         }
-    }
-
-    public static void main(String[] args) {
-        // Carregar contatos do arquivo no início da execução
-        List<Contato> contatos = AgendaManipulacao.carregarDeArquivo();
-
-        // Criar a instância da Agenda com os contatos carregados
-        Agenda agenda = new Agenda(contatos);
-
-        Scanner scanner = new Scanner(System.in);
-
-        int escolha;
-        do {
-            System.out.println("##################");
-            System.out.println("##### AGENDA #####");
-//            System.out.println(">>>> Contatos <<<<");
-//            agenda.exibirContatos();
-            System.out.println(">>>> Menu <<<<");
-            System.out.println("1 - Adicionar Contato");
-            System.out.println("2 - Remover Contato");
-            System.out.println("3 - Editar Contato");
-            System.out.println("4 - Sair");
-
-            escolha = scanner.nextInt();
-            scanner.nextLine(); // Consumir a quebra de linha
-
-            switch (escolha) {
-                case 1:
-                    // Lógica para adicionar contato
-                    Contato novoContato = adicionarContato(scanner);
-                    agenda.adicionarContato(novoContato);
-                    break;
-                case 2:
-                    // Lógica para remover contato
-                    System.out.println("Informe o ID do contato a ser removido: ");
-                    Long idRemover = scanner.nextLong();
-                    agenda.removerContato(idRemover);
-                    break;
-                case 3:
-                    // Lógica para editar contato
-                    System.out.println("Informe o ID do contato a ser editado: ");
-                    Long idEditar = scanner.nextLong();
-                    scanner.nextLine();
-                    agenda.editarContato(idEditar, scanner);
-                    break;
-                case 4:
-                    // Salvar contatos no arquivo antes de sair
-                    AgendaManipulacao.salvarEmArquivo(agenda.getContatos());
-                    System.out.println("Saindo...");
-                    break;
-                default:
-                    System.out.println("Opção inválida. Tente novamente.");
-            }
-        } while (escolha != 4);
-
-        scanner.close();
-    }
-
-    private static Contato adicionarContato(Scanner scanner) {
-        System.out.println("Informe o nome do contato: ");
-        String nome = scanner.nextLine();
-
-        System.out.println("Informe o sobrenome do contato: ");
-        String sobrenome = scanner.nextLine();
-
-        // Criar um novo objeto Contato
-        Contato novoContato = new Contato(nome, sobrenome);
-
-        // Adicionar telefones
-        System.out.println("Adicionar telefones (Digite '0' para encerrar): ");
-        while (true) {
-            System.out.println("Informe o DDD do telefone (ou 0 para encerrar): ");
-            String ddd = scanner.nextLine();
-
-            if (ddd.equals("0")) {
-                break;
-            }
-
-            System.out.println("Informe o número do telefone: ");
-            Long numero = Long.parseLong(scanner.nextLine());
-
-            Telefone telefone = new Telefone(ddd, numero);
-            novoContato.adicionarTelefone(telefone);
-        }
-
-        return novoContato;
     }
 }
--- src/br/com/projeto/agenda/Main.java
+++ src/br/com/projeto/agenda/Main.java
@@ -2,14 +2,107 @@ package br.com.projeto.agenda;
 
 import br.com.projeto.agenda.io.AgendaManipulacao;
 import br.com.projeto.agenda.modelo.Contato;
+import br.com.projeto.agenda.modelo.Telefone;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
+    // Códigos de cores ANSI
+    public static final String RESET = "\u001B[0m";
+    public static final String BLACK = "\u001B[30m";
+    public static final String RED = "\u001B[31m";
+    public static final String GREEN = "\u001B[32m";
+    public static final String YELLOW = "\u001B[33m";
+    public static final String BLUE = "\u001B[34m";
+    public static final String PURPLE = "\u001B[35m";
+    public static final String CYAN = "\u001B[36m";
+    public static final String WHITE = "\u001B[37m";
 
+    public static void main(String[] args) {
+        // Carregar contatos do arquivo no início da execução
+        List<Contato> contatos = AgendaManipulacao.carregarDeArquivo();
+
+        // Criar a instância da Agenda com os contatos carregados
+        Agenda agenda = new Agenda(contatos);
+
+        Scanner scanner = new Scanner(System.in);
+
+        int escolha;
+        do {
+            System.out.println(BLUE + "##################");
+            System.out.println(BLUE + "##### AGENDA #####");
+//            System.out.println(">>>> Contatos <<<<");
+//            agenda.exibirContatos();
+            System.out.println(PURPLE + " >>>> Menu <<<<" + RESET);
+            System.out.println(GREEN + "1" + RESET + " - Adicionar Contato");
+            System.out.println(GREEN + "2" + RESET + " - Remover Contato");
+            System.out.println(GREEN + "3" + RESET + " - Editar Contato");
+            System.out.println(GREEN + "4" + RESET + " - Salvar e Sair");
+
+            escolha = scanner.nextInt();
+            scanner.nextLine(); // Consumir a quebra de linha
+
+            switch (escolha) {
+                case 1:
+                    // Lógica para adicionar contato
+                    Contato novoContato = adicionarContato(scanner);
+                    agenda.adicionarContato(novoContato);
+                    break;
+                case 2:
+                    // Lógica para remover contato
+                    System.out.println(YELLOW + "Informe o ID do contato a ser removido: ");
+                    Long idRemover = scanner.nextLong();
+                    agenda.removerContato(idRemover);
+                    break;
+                case 3:
+                    // Lógica para editar contato
+                    System.out.println(YELLOW + "Informe o ID do contato a ser editado: ");
+                    Long idEditar = scanner.nextLong();
+                    scanner.nextLine();
+                    agenda.editarContato(idEditar, scanner);
+                    break;
+                case 4:
+                    // Salvar contatos no arquivo antes de sair
+                    AgendaManipulacao.salvarEmArquivo(agenda.getContatos());
+                    System.out.println(YELLOW + "Saindo...");
+                    break;
+                default:
+                    System.out.println(RED + "Opção inválida. Tente novamente.");
+            }
+        } while (escolha != 4);
+
+        scanner.close();
+    }
+
+    private static Contato adicionarContato(Scanner scanner) {
+        System.out.println(GREEN + "Informe o nome do contato: ");
+        String nome = scanner.nextLine();
+
+        System.out.println(GREEN + "Informe o sobrenome do contato: ");
+        String sobrenome = scanner.nextLine();
+
+        // Criar um novo objeto Contato
+        Contato novoContato = new Contato(nome, sobrenome);
+
+        // Adicionar telefones
+        System.out.println(GREEN + "Adicionar telefones " + YELLOW + "(Digite '0' para encerrar): ");
+        while (true) {
+            System.out.println(GREEN + "Informe o DDD do telefone " + YELLOW + "(ou 0 para encerrar): ");
+            String ddd = scanner.nextLine();
+
+            if (ddd.equals("0")) {
+                break;
+            }
+
+            System.out.println(GREEN + "Informe o número do telefone: ");
+            Long numero = Long.parseLong(scanner.nextLine());
+
+            Telefone telefone = new Telefone(ddd, numero);
+            novoContato.adicionarTelefone(telefone);
         }
 
-
+        return novoContato;
+    }
 
 }
